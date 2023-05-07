5dcc9e486a27975b010a3b7c47dc46ba5e9afb8f
EMMANUEL VILLADA SUAREZ <emmanuelvillada310016@correo.itm.edu.co>
2023-05-07 11:58:55 -0500

    migration

---
+++ Parcial3/Parcial3/Controllers/ServiceController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Parcial3.Controllers
+{
+    public class ServiceController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
