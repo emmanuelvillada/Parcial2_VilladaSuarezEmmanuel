a7ff1d07501d44a180a1cc874f7edd1556d8ebd9
EMMANUEL VILLADA SUAREZ <emmanuelvillada310016@correo.itm.edu.co>
2023-05-08 12:58:18 -0500

    programn fixed

--- Parcial3/Parcial3/Program.cs
+++ Parcial3/Parcial3/Program.cs
@@ -59,7 +59,7 @@ void SeederData()
 		SeederDb? service = scope.ServiceProvider.GetService<SeederDb>();
 		service.SeederAsync().Wait();
 	}
-
+}
 	// Configure the HTTP request pipeline.
 	if (!app.Environment.IsDevelopment())
 	{
@@ -67,6 +67,7 @@ void SeederData()
 		// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 		app.UseHsts();
 	}
+
 
 	app.UseHttpsRedirection();
 	app.UseStaticFiles();
@@ -80,4 +81,4 @@ void SeederData()
 		pattern: "{controller=Home}/{action=Index}/{id?}");
 
 	app.Run();
-}
+
